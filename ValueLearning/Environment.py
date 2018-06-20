master: Switched to reporting deviation from optimal distance as decided and tested again with a random agent - Value function seems correct now. Only remaining check is theoritical calculation of the value function and its comparison with the estimated one.
Changes committed:
	modified:   Environment.py
	modified:   TODO
	modified:   ValueLearning.py

ValueLearning/Environment.py
@@ -12,12 +12,13 @@ class Maze(object):
     # There can be a reward(s) associated with both the goal and non-goal states.
     GOAL_STATE_REWARD = 0.05
     NON_GOAL_STATE_REWARD = -0.0001
+    MOVE_DISTANCE = 0.2
 
     def __init__(self, nx, ny):
         self._nx = nx
         self._ny = ny
         self._state = [0, 0]
-        self._action_map = {'E':(-0.2, 0), 'W':(0.2,0), 'N':(0,0.2), 'S':(0,-0.2)}
+        self._action_map = {'E':(-self.MOVE_DISTANCE, 0), 'W':(self.MOVE_DISTANCE,0), 'N':(0,self.MOVE_DISTANCE), 'S':(0,-self.MOVE_DISTANCE)}
         self._n_states = (2+self._nx) * (2+self._ny)
     
         # Placeholders for the goal location(s) and initial location(s)
@@ -158,7 +159,10 @@ class Maze(object):
         raise NotImplementedError()
 
     def reachedGoalState(self):
-        raise NotImplementedError
+        raise NotImplementedError()
+
+    def getOptimalStepsToGoal(self):
+        raise NotImplementedError()
 
 class RandomGoalOpenField(Maze):
     def __init__(self, nx, ny):
@@ -175,6 +179,11 @@ class RandomGoalOpenField(Maze):
         # There is just ONE goal location, nothing complicated here
         goal_location = self._goal_locations[0]
         return ((pow(self._state[0] - goal_location[0], 2) + pow(self._state[1] - goal_location[1], 2) < pow(self._goal_th, 2)))
+
+    def getOptimalStepsToGoal(self):
+        goal_location = self._goal_locations[0]
+        init_location = self._init_locations[0]
+        return np.round((abs(goal_location[0]-init_location[0])+abs(goal_location[1]-init_location[1]))/self.MOVE_DISTANCE)
 
 class Wall(object):
     """

ValueLearning/ValueLearning.py
@@ -33,8 +33,8 @@ def learnValueFunction(n_trials, environment, place_cells, actor=None, critic=No
 
     if actor is None:
         # actor = Agents.Actor(environment.getActions(), len(place_cells)) 
-        # actor = Agents.RandomAgent(environment.getActions(), len(place_cells)) 
-        actor = Agents.IdealActor(environment, critic, place_cells)
+        actor = Agents.RandomAgent(environment.getActions(), len(place_cells)) 
+        # actor = Agents.IdealActor(environment, critic, place_cells)
     else:
         assert(actor.getNFields() == len(place_cells))
 
@@ -43,7 +43,12 @@ def learnValueFunction(n_trials, environment, place_cells, actor=None, critic=No
         # Path is visualized using a graphics object
         canvas = Graphics.WallMazeCanvas(environment)
 
+        # Initialize a new location and adjust for the optimal number of steps
+        # needed to get to the goal.
         environment.redrawInitLocation()
+        optimal_steps_to_goal = environment.getOptimalStepsToGoal()
+        n_steps[trial] = -optimal_steps_to_goal
+
         initial_state = environment.getCurrentState()
         canvas.update(initial_state)
         while not environment.reachedGoalState():
@@ -92,7 +97,6 @@ def learnValueFunction(n_trials, environment, place_cells, actor=None, critic=No
                 critic_weights = np.reshape(critic.getWeights(), -1)
                 Graphics.histogram(critic_weights)
                 """
-
 
     if (DBG_LVL > 0):
         Graphics.plot(n_steps)