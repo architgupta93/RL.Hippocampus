b592cc0b611d39ce70968583c6991d9dd958fab8
Archit Gupta <architgupta@berkeley.edu>
2018-06-27 13:18:57 -0700

    non-trivial-mazes: Added PCA analysis to the multi-environment setup.
    Changes committed:
    	modified:   Examples/multi_maze_random_agent.py

--- ValueLearning/Examples/multi_maze_random_agent.py
+++ ValueLearning/Examples/multi_maze_random_agent.py
@@ -5,31 +5,35 @@ import Graphics
 import Agents
 
 # Packages for visualization and analysis
+from sklearn.decomposition import PCA
 import numpy as np
+import matplotlib.pylab as plt
 
 def testMaze():
     """
     No comments here. Look at single_maze_learning_agent.py for more details!
     """
-    ValueLearning.DBG_LVL = 1
+    ValueLearning.DBG_LVL = 0
 
-    nx = 4
-    ny = 5
+    nx = 6
+    ny = 6
 
     # Set the number of cells to be used per "place field" - Same for all the environments
-    Hippocampus.N_CELLS_PER_FIELD = 1
+    Hippocampus.N_CELLS_PER_FIELD = 64
 
     n_fields = round(1.0 * (nx + 3) * (ny+3))
     n_cells  = Hippocampus.N_CELLS_PER_FIELD * n_fields
 
-    n_training_trials = 200
-    n_alternations = 4
-    max_train_steps = 200
+    n_training_trials = 1
+    n_alternations = 100
+    max_train_steps = 1000
 
     # First Environment: Has its own place cells and place fields
     env_E1          = Environment.RandomGoalOpenField(nx, ny)
     place_fields_E1 = Hippocampus.setupPlaceFields(env_E1, n_fields)
     place_cells_E1  = Hippocampus.assignPlaceCells(n_cells, place_fields_E1)
+
+    # Train a critic on the first environment
 
     # Create empty actors and critics
     actor = Agents.RandomAgent(env_E1.getActions(), n_cells)
@@ -46,20 +50,46 @@ def testMaze():
     # way the agent behaves.
     learning_steps_E1 = np.zeros((n_alternations, 1), dtype=float)
     learning_steps_E2 = np.zeros((n_alternations, 1), dtype=float)
+
+    # keep track of weights for PCA
+    weights        = np.empty((n_alternations * 2, n_cells), dtype=float)
     for alt in range(n_alternations):
         print('Alternation: %d' % alt)
         # First look at the performance of the agent in the task before it is
         # allowed to learn anything. Then allow learning
-
         print('Learning Environment A')
         (actor, critic, steps_E1) = ValueLearning.learnValueFunction(n_training_trials, env_E1, place_cells_E1, actor, critic, max_train_steps)
         learning_steps_E1[alt] = np.mean(steps_E1)
+        weights[2*alt, :] = critic.getWeights()
 
         # Repeat for environment 1
         print('Learning Environment B')
         (actor, critic, steps_E2) = ValueLearning.learnValueFunction(n_training_trials, env_E2, place_cells_E2, actor, critic, max_train_steps)
         learning_steps_E2[alt] = np.mean(steps_E2)
+        weights[2*alt + 1, :] = critic.getWeights()
 
+    # Decompose the weight sequence into its principal components
+    components = PCA(n_components = 2)
+
+    # Fit only the last few entries
+    # components.fit(weights[-training_eps:-1,:])
+
+    # Fit all the entries
+    components.fit(weights)
+
+    """ DEBUG
+    print(components.explained_variance_ratio_)
+    print(components.singular_values_)
+    """
+
+    # Get the decomposition of the weight vectors into the constituents 
+    transformed_weights = components.transform(weights)
+    plt.figure()
+    plt.scatter(transformed_weights[:, 0], transformed_weights[:, 1], c=range(2*n_alternations), \
+        cmap='viridis', marker='d', alpha=0.5)
+    plt.colorbar()
+    plt.grid()
+    plt.show()
     # canvas_E1.plotValueFunction(place_cells_E1, critic)
     # canvas_E2.plotValueFunction(place_cells_E2, critic)
 
