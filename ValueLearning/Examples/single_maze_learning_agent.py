841b4a75ec595edb12e77bde70a0e59a2beae115
Archit Gupta <architgupta@berkeley.edu>
2019-05-30 11:24:57 -0700

    master: Added lots of debugging statements while trying to figure out the best way to use multiprocessing instead of thread (which happen to be a lot slower.)
    Changes committed:
    	modified:   single_maze_learning_agent.py

--- ValueLearning/Examples/single_maze_learning_agent.py
+++ ValueLearning/Examples/single_maze_learning_agent.py
@@ -6,10 +6,12 @@ import Graphics
 # Packages for local plotting/analysis
 import time
 import threading
+import multiprocessing
 import numpy as np
-import matplotlib.pylab as pl
+import matplotlib.pyplot as pl
+from pprint import pprint
 
-def testMaze(nT, nN, learning_dbg_lvl=0, navigation_dbg_lvl=2):
+def testMaze(nT, nN, learning_dbg_lvl=0, navigation_dbg_lvl=0):
     ValueLearning.DBG_LVL = learning_dbg_lvl
     # Create a Maze for the experiment
     nx = 6
@@ -39,10 +41,12 @@ def testMaze(nT, nN, learning_dbg_lvl=0, navigation_dbg_lvl=2):
 
     # Learn how to navigate this Environment
     (actor, critic, learning_steps) = ValueLearning.learnValueFunction(nT, maze, place_cells, max_steps=4000)
+    print(learning_steps)
 
     # Try a single trial on the same Maze and see how we do
     ValueLearning.DBG_LVL = navigation_dbg_lvl
     navigation_steps = ValueLearning.navigate(nN, maze, place_cells, actor, critic, max_steps=400)
+    print(navigation_steps)
     return (learning_steps, navigation_steps)
 
 class MazeThread(threading.Thread):
@@ -62,7 +66,7 @@ class MazeThread(threading.Thread):
 
 if __name__ == "__main__":
     # For reasonable data
-    n_epochs = 1
+    n_epochs = 3
     n_training_trials = 100 # Training trials
     n_navigation_trials = 20  # Navigation trials
 
@@ -93,8 +97,12 @@ if __name__ == "__main__":
         t.join()
 
     for epoch in range(n_epochs):
+        pprint(threads[epoch].training_steps)
         training_steps[:, epoch] = threads[epoch].training_steps
         navigation_steps[:, epoch] = threads[epoch].navigation_steps
+
+    # for t in threads:
+    #     t.terminate()
 
     mean_training_steps   = np.reshape(np.mean(training_steps, axis=1), (n_training_trials, 1))
     mean_navigation_steps = np.reshape(np.mean(navigation_steps, axis=1), (n_navigation_trials, 1))
@@ -114,6 +122,10 @@ if __name__ == "__main__":
     err_training_steps = np.std(training_steps, axis=1)
     err_navigation_steps = np.std(navigation_steps, axis=1)
 
+    # Print all the data before plotting
+    print('%d Training trials'%n_training_trials)
+    pprint(mean_training_steps)
+
     training_fig = pl.figure()
     training_ax = training_fig.add_subplot(111)
     training_ax.errorbar(range(n_training_trials), mean_training_steps, yerr=err_training_steps, marker='d', ecolor='black', capsize=0.5)
@@ -121,6 +133,9 @@ if __name__ == "__main__":
     training_ax.set_ylabel('Latency')
     training_ax.grid(True)
     pl.show()
+
+    print('%d Navigation trials'%n_navigation_trials)
+    pprint(mean_navigation_steps)
 
     navigation_fig = pl.figure()
     navigation_ax = navigation_fig.add_subplot(111)
